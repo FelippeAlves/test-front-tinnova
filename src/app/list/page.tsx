eedf2a64c95d05385e03305bf8865c3bf337067c
Felippe <felippealves17@gmail.com>
2025-04-04 10:41:07 -0300

    feat: seting default environment

---
+++ src/app/list/page.tsx
@@ -0,0 +1,5 @@
+export default function ListPage() {
+    return (
+        <h1>ListPage</h1>
+    )
+}
---
+++ src/components/menu.tsx
@@ -0,0 +1,7 @@
+export default function Menu() {
+    return <ul className="menu">
+        <li>Home</li>
+        <li>Cadastro</li>
+        <li>Lista</li>
+    </ul>
+}
